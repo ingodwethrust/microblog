6573089c2b5de4ba0560623e4d4dbc94aa287218
Antonydze <antony.metrica@yandex.ru>
2019-05-17 21:36:17 +0300

    Initial version of app

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+# microblog app with distributed architecture 
+
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from app import routes
